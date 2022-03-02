edaa2256b6af2769bd431d94efc0f7c81f0d165f
DannyHep <90182872+DannyHep@users.noreply.github.com>
2022-03-02 15:59:20 +0000

    codersGuild projects

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// --------------------------Coder's guild - Projects 1 ------------------
+// ----------------------------Coder's guild ------------------
+// -------------------------JavaScript Projects 1-------------------
 
 // Age Calculator
 const birthYear = 1993;
@@ -29,7 +30,7 @@ const fahrenheitToCelcius = ((fahrenheit - 32) * 5) / 9 + "\u00B0C";
 console.log(`${tempCelcius}"\u00B0C" is ${celciusToFahrenheit}`);
 console.log(`${fahrenheit}"\u00B0F" is ${fahrenheitToCelcius}`);
 
-// --------------------------Project 2 - Functions and Control Flow ------------------
+// --------------------------JavaScrip Project 2 - Functions and Control Flow ------------------
 
 // What number is bigger?
 const num1 = Math.random() * 100;
@@ -121,13 +122,114 @@ function pluralise(num, animal) {
 
 pluralise(5, "sheep");
 
-
 // mixUp
-function mixUp(string1, string2){
-const stringOneMixed = string2.slice(0 ,2) + string1.slice(2, string1.length);
-const stringTwoMixed = string1.slice(0 ,2) + string2.slice(2, string2.length);
+function mixUp(string1, string2) {
+  const stringOneMixed = string2.slice(0, 2) + string1.slice(2, string1.length);
+  const stringTwoMixed = string1.slice(0, 2) + string2.slice(2, string2.length);
 
-return stringTwoMixed + " " + stringOneMixed;
+  return stringTwoMixed + " " + stringOneMixed;
 }
 
-console.log(mixUp("hello", "world"))
+console.log(mixUp("hello", "world"));
+
+// -----------------JavaScript Projects 3 -  Loops, Arrays, and Objects --------------------
+
+// Even/odd counter
+for (let i = 0; i < 21; i++) {
+  if (i % 2 === 0) {
+    console.log("even", i);
+  } else {
+    console.log("odd", i);
+  }
+}
+
+// The Reading List
+let readingList = [
+  {
+    title: "The Last Kingdom Series",
+    author: "Bernard Cornwell",
+    readStatus: true,
+  },
+  {
+    title: "The Hitch Hikers Guide to the Galaxy",
+    author: "Douglas Adams",
+    readStatus: true,
+  },
+  {
+    title: "Emperor Series",
+    author: "Conn Iggulden",
+    readStatus: true,
+  },
+  {
+    title: "Harry Potter and the Prisoner of Askaban",
+    author: "J.K Rowling",
+    readStatus: true,
+  },
+  {
+    title: "One Flew Over the Coockoo's Nest",
+    author: "Ken Kesey",
+    readStatus: true,
+  },
+  {
+    title: "Game of Thrones Series",
+    author: "George R Martin",
+    readStatus: false,
+  },
+];
+
+for (let i = 0; i < readingList.length; i++) {
+  if (readingList[i].readStatus === true) {
+    console.log(
+      `You already read ${readingList[i].title} by ${readingList[i].author}`
+    );
+  } else if (readingList[i].readStatus === false) {
+    console.log(
+      `You still need to read ${readingList[i].title} by ${readingList[i].author}`
+    );
+  }
+}
+
+// Recipe
+let favRecipe = 
+  {
+    recipeTitle: "Hairy Biker's Chicken Korma",
+    servings: 2,
+    ingredients: [
+      "Onion",
+      "Chilli Powder",
+      "Chicken",
+      "Natural Yoghurt",
+      "Cumin",
+      "Turmeric",
+      "Coriander",
+      "Cardamon pods",
+      "garlic",
+      "cloves",
+      "Bay leaf",
+      "cream",
+    ],
+    directions:
+      "slice onions, and cook on medium heat. One cooked, add spice mix prepared in a mortar and pestle. Add cream and season for taste.",
+    letsCook: function() {
+      console.log(`I'm hungry!Let's cook ${this.recipeTitle}`)
+    }
+  };
+
+function recipeIngredientList() {
+  for (let i = 0; i < favRecipe.ingredients.length; i++) {
+    console.log(favRecipe.ingredients[i])
+  }
+}
+
+recipeIngredientList();
+
+
+// Function
+favRecipe.letsCook();
+
+
+// Times Tables
+for (let i = 0; i < 13; i++){
+  console.log(9 * i)
+};
+
